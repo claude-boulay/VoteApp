6098f16eedf6e83a7f31ec47fb668a2f15f992de
Boulay <cbmagala@gmail.com>
2025-02-04 11:22:51 +0100

    boucle pour le reset

--- voting-app/reset.bash
+++ voting-app/reset.bash
@@ -1,2 +1,4 @@
-#permets de vider la tables votes
-docker exec -it $(docker ps -q -f name=vote-app_postgres) psql -U postgres -d postgres -c "DELETE FROM votes;"
+#permets de vider la tables votes pour tous les répliquas du service
+for container in $(docker ps -q -f name=mon_app_postgres); do
+  docker exec -it "$container" psql -U postgres -d postgres -c "DELETE FROM votes;"
+done
