82ca9a60edfff8f702316484a4a81f978610c29a
Boulay <cbmagala@gmail.com>
2025-02-08 09:32:03 +0100

    correction du nom du service

--- voting-app/reset.bash
+++ voting-app/reset.bash
@@ -1,4 +1,4 @@
 #permets de vider la tables votes pour tous les répliquas du service
-for container in $(docker ps -q -f name=mon_app_postgres); do
+for container in $(docker ps -q -f name=vote-app_postgres); do
   docker exec -it "$container" psql -U postgres -d postgres -c "DELETE FROM votes;"
 done
